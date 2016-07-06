eacacd959e6a23ecc0bf3dba7baac91dd2b60e8b
Richard Plotkin <richardjplotkin@gmail.com>
2016-07-06 14:18:12 -0400

    routes to satisfy initial API Blueprint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/')
+  .get(function(req, res) {
+    User
+      .fetchAll()
+      .then(function(users) {
+        res.json(users);
+      })
+  });
+
+router.route('/:id')
+  .get(function(req, res) {
+    //fetch the user by id
+    User
+      .where({id: req.params.id})
+      .fetch({
+        withRelated: ['emails']
+      })
+      .then(function(user) {
+        //serialize
+        user = user.serialize({shallow:false});
+        user.emails = user.emails.map(obj => obj.email).sort();
+        res.json(user);
+      });
+  });
 
 module.exports = router;
